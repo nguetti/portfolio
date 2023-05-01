4b81dd1a0b21a38329165d19250182577c9776d6
Nicanor <nicanorguetti@gmail.com>
2023-04-30 22:05:43 -0300

    primer commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import { NavBar } from './components/NavBar';
+import { Banner } from './components/Banner';
+import { Skills } from './components/Skills' ;
+import { Projects } from './components/Projects' ;
+import { Footer } from './components/Footer';
+
+// import { Contact } from './components/Contact';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      <Banner />
+      <Skills />
+      <Projects />
+      {/* <Contact /> */}
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
